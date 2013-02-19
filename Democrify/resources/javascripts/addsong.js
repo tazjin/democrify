e4507c4100c6a04dbe763f436d68ed0a57c75b81
Vincent Ambo <vincent@spotify.com>
2013-02-20 00:56:14 +0100

    * run search when enter is pressed

--- Democrify/resources/javascripts/addsong.js
+++ Democrify/resources/javascripts/addsong.js
@@ -1,19 +1,28 @@
-$(function(){
-    // search implementation
-    $('#searchbutton').click(function(e){
-        var searchtype = $('#searchtype option:selected').val();
-        var searchterm = $('#search').val();
-        $('#resultcontainer').empty();
-        $.getJSON(('http://ws.spotify.com/search/1/' + searchtype + '.json?q=' + searchterm), function(data){
-            $.each(data.tracks.slice(0, 10), function(k, track){
-                var trackId = track.href.replace('spotify:track:', '');
-                $('#resultcontainer').append('<div class="row"><div class="two columns mobile-one"><a href="#" id="'+trackId+'" class="addtrack"><img src="http://placehold.it/80x80&text=ADD"></a></div><div class="ten columns mobile-three resultspans"><span class="track">'+track.name+'</span><br /><span class="artist">by '+track.artists[0].name+'</span></div><br /><hr /></div>');
-                $('#' + trackId).click(function(t){
-                    $.get('/add/' + trackId);
-                    $(this).attr('id', 'added');
-                    $(this).children('img').attr('src', 'http://placehold.it/80x80&text=ADDED');
-                });
+function runSearch(e){
+    var searchtype = $('#searchtype option:selected').val();
+    var searchterm = $('#search').val();
+    $('#resultcontainer').empty();
+    $.getJSON(('http://ws.spotify.com/search/1/' + searchtype + '.json?q=' + searchterm), function(data){
+        $.each(data.tracks.slice(0, 10), function(k, track){
+            var trackId = track.href.replace('spotify:track:', '');
+            $('#resultcontainer').append('<div class="row"><div class="two columns mobile-one"><a href="#" id="'+trackId+'" class="addtrack"><img src="http://placehold.it/80x80&text=ADD"></a></div><div class="ten columns mobile-three resultspans"><span class="track">'+track.name+'</span><br /><span class="artist">by '+track.artists[0].name+'</span></div><br /><hr /></div>');
+            $('#' + trackId).click(function(t){
+                $.get('/add/' + trackId);
+                $(this).attr('id', 'added');
+                $(this).children('img').attr('src', 'http://placehold.it/80x80&text=ADDED');
             });
         });
+    });
+}
+
+$(function(){
+    // Register search on button click
+    $('#searchbutton').click(runSearch);
+
+    //Register search on enter
+    $('#search').keypress(function(e){
+        if (e.which == 13){
+            runSearch(e);
+        }
     });
 });
