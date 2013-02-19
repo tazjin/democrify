1577f00f15a08f29e951b978c072e04a73ac2305
Vincent Ambo <vincent@spotify.com>
2013-02-19 16:56:37 +0100

    * added WebAPI.hs
    WebAPI.hs contains a simple function to lookup a song by ID from the
    Spotify API

---
+++ Democrify/WebAPI.hs
@@ -0,0 +1,36 @@
+{-| Very "to-the-point" bindings to the Spotify Web API
+-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module WebAPI (identifyTrack) where
+
+--import Acid
+import           Acid
+import           Control.Applicative  (pure, (<$>), (<*>))
+import           Data.Aeson
+import           Data.Text            (Text, unpack)
+import           Network.HTTP.Conduit
+
+
+instance FromJSON (Text -> SpotifyTrack) where
+    parseJSON (Object v) = do
+        o <- v .: "track"
+        (a:_) <- o .: "artists"
+        SpotifyTrack <$> pure 0
+                     <*> a .: "name"
+                     <*> o .: "name"
+
+-- |Requests a track ID from the Spotify lookup service
+identifyTrack :: Text -> IO (Maybe SpotifyTrack)
+identifyTrack trackId = do
+    r <- simpleHttp $ trackURL trackId
+    case decode r of
+        Nothing -> return Nothing
+        Just f  -> return $ Just $ f trackId
+
+-- |Spotify API URL to track metadata (in JSON)
+trackURL :: Text -- ^ Track ID
+         -> String
+trackURL t = let trackId = unpack t
+             in  "http://ws.spotify.com/lookup/1/.json?uri=spotify:track:" ++ trackId
